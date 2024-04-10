b32fae71cda05873f6b58659ba20e972cee0fb19
sankar mukesh <ysankarmukesh@gmail.com>
2024-04-11 00:02:38 +0530

    intial setuo

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -1,6 +1,8 @@
 
+const Project = require("../models/Projects")
 const Tasks = require("../models/Tasks")
 const UserStories = require("../models/UserStories")
+const send_mail = require("../helpers/EmailSending");
 
 
 exports.getAllTasks = async (req, res, next) => {
@@ -38,6 +40,10 @@ exports.addTask = async (req, res, next) => {
         userStory.taskIds.unshift(addedTask._id.toString())
         userStory.save()
         const fetchingNewTask = await Tasks.findOne({ _id: addedTask._id }).populate({ path: 'lastUpdatedBy', select: ["userName", "_id", 'email', 'image'] }).populate({ path: 'owner', select: ["userName", "_id", 'email', 'image'] })
+        const projectDetails = await Project.findOne({ _id: projectId })
+        const userStorydetails = await UserStories.findOne({ _id: userStoryId })
+
+        await send_mail(fetchingNewTask.owner.email, 'Assigned a Task for you!', `Hi ${fetchingNewTask.owner.userName}, a Task <b>${fetchingNewTask.name}</b> has been assigned to you from project <b>${projectDetails.name}</b> and userStory <b>${userStorydetails.name}</b>`)
         return res.status(200).json(fetchingNewTask)
     } catch (err) {
         return res.status(400).send(err)
@@ -54,6 +60,11 @@ exports.updateTask = async (req, res, next) => {
             return res.status(400).json({ message: 'Task not exists' })
         }
         await Tasks.updateOne({ _id: taskId }, { $set: { owner: owner, name: name, description: description, status: status, lastUpdatedBy: updatedBy, due: due } })
+        const updatedTask = await Tasks.findOne({ _id: taskId }).populate({ path: 'owner', select: ["userName", "_id", 'email', 'image'] })
+        const projectDetails = await Project.findOne({ _id: updatedTask.projectId })
+        const userStorydetails = await UserStories.findOne({ _id: updatedTask.userStoryId })
+        await send_mail(updatedTask.owner.email, 'Updates on your task!', `Hi ${updatedTask.owner.userName}, a Task <b>${updatedTask.name}</b> has been updated in project <b>${projectDetails.name}</b> and userStory <b>${userStorydetails.name}</b>`)
+
         return res.status(200).send('userstory updated')
     } catch (err) {
         return res.status(400).send(err)
--- controllers/userStoryController.js
+++ controllers/userStoryController.js
@@ -1,12 +1,15 @@
 const UserStories = require("../models/UserStories")
 const Tasks = require("../models/Tasks")
+const Project = require("../models/Projects")
+
+const send_mail = require("../helpers/EmailSending");
 
 
 exports.getUserStoryBasedOnProject = async (req, res, next) => {
     try {
         const {projectId} = req.body
         const userStories = await UserStories.find({ projectId: projectId }).populate({ path: 'lastUpdatedBy', select: ["userName", "_id", 'email', 'image'] }).populate({ path: 'owner', select: ["userName", "_id", 'email', 'image'] }).populate({
-            path: 'taskIds', select: ["name", "projectId", "createdBy", 'description', 'due', 'owner', 'status', 'updatedAt'], populate: {
+            path: 'taskIds', select: ["name", "projectId", "createdBy", 'description', 'due', 'owner', 'status', 'updatedAt', 'lastUpdatedBy'], populate: {
                 path: 'owner', // Path to populate within each task object
                 select: ["userName", "_id", 'email', 'image'] // Fields to select from the owner object
             }, 
@@ -22,7 +25,7 @@ exports.getSingleUserStory = async (req, res, next) => {
     try {
         const { userStoryId } = req.body
         const userStories = await UserStories.findOne({ _id: userStoryId }).populate({ path: 'lastUpdatedBy', select: ["userName", "_id", 'email', 'image'] }).populate({ path: 'owner', select: ["userName", "_id", 'email', 'image'] }).populate({
-            path: 'taskIds', select: ["name", "projectId", "createdBy", 'description', 'due', 'owner', 'status', 'updatedAt'], populate: {
+            path: 'taskIds', select: ["name", "projectId", "createdBy", 'description', 'due', 'owner', 'status', 'updatedAt', 'lastUpdatedBy'], populate: {
                 path: 'owner', // Path to populate within each task object
                 select: ["userName", "_id", 'email', 'image'] // Fields to select from the owner object
             },  })
@@ -42,10 +45,13 @@ exports.addUserStory = async (req, res, next) => {
         }
         const addedData = await UserStories.create({ projectId: projectId, name: name, description, owner, createdBy: user_id, status: 'New', lastUpdatedBy: user_id })
         const result = await UserStories.findOne({ _id: addedData._id }).populate({ path: 'lastUpdatedBy', select: ["userName", "_id", 'email', 'image'] }).populate({ path: 'owner', select: ["userName", "_id", 'email', 'image'] }).populate({
-            path: 'taskIds', select: ["name", "projectId", "createdBy", 'description', 'due', 'owner', 'status', 'updatedAt'], populate: {
+            path: 'taskIds', select: ["name", "projectId", "createdBy", 'description', 'due', 'owner', 'status', 'updatedAt', 'lastUpdatedBy'], populate: {
                 path: 'owner', // Path to populate within each task object
                 select: ["userName", "_id", 'email', 'image'] // Fields to select from the owner object
-            },  })
+            },
+        })
+        const projectDetails = await Project.findOne({ _id: projectId})
+        await send_mail(result.owner.email, 'Assigned a user story!', `Hi ${result.owner.userName}, a user story <b>${result.name}</b> has been assigned to you from project <b>${projectDetails.name}</b>`)
         return res.status(200).json(result)
     } catch (err) {
         return res.status(400).send(err)
@@ -63,10 +69,15 @@ exports.updateOwnerForUserStory = async (req, res, next) => {
         }
         await UserStories.updateOne({ _id: userStoryid }, { $set: { owner: owner, name: name, description: description, status: status, lastUpdatedBy: updatedBy } })
         const result = await UserStories.findOne({ _id: userStoryid }).populate({ path: 'lastUpdatedBy', select: ["userName", "_id", 'email', 'image'] }).populate({ path: 'owner', select: ["userName", "_id", 'email', 'image'] }).populate({
-            path: 'taskIds', select: ["name", "projectId", "createdBy", 'description', 'due', 'owner', 'status', 'updatedAt'], populate: {
+            path: 'taskIds', select: ["name", "projectId", "createdBy", 'description', 'due', 'owner', 'status', 'updatedAt', 'lastUpdatedBy'], populate: {
                 path: 'owner', // Path to populate within each task object
                 select: ["userName", "_id", 'email', 'image'] // Fields to select from the owner object
-            },  })
+            },
+        })
+        
+        const projectDetails = await Project.findOne({ _id: result.projectId })
+        await send_mail(result.owner.email, 'Updates on your user Story!', `Hi ${result.owner.userName}, user story <b>${result.name}</b> has been updated in project <b>${projectDetails.name}</b>`)
+
         return res.status(200).json(result)
     } catch (err) {
         return res.status(400).send(err)
